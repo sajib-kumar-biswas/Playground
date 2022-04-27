8b0c65756afabdd32cbd7fe4713db20513e086fc
sajib-kumar-biswas <sajibbiswas01521457841@gmail.com>
2022-04-27 14:14:20 +0600

    useState Hook

--- my-playground/src/Components/ReactHook.js
+++ my-playground/src/Components/ReactHook.js
@@ -1,10 +1,26 @@
+import { useState } from "react"
+import styled from 'styled-components'
+
+const Button = styled.button`
+    width: 50vw;
+    height: 20vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background: teal;
+`
 
 function ReactHook() {
-
-    
+  const [count, setCount] = useState(0);
 
   return (
-    <div>ReactHook</div>
+    <>
+      <h2>{count}</h2>
+      <Button onClick={() => setCount(0)}>Reset</Button>
+      <Button onClick={() => setCount((prevCount) => { return prevCount - 1 })}>-</Button>
+      <Button onClick={() => setCount(prevCount => prevCount + 1)}>+</Button>
+    </>
   )
 }
 
